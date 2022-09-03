b19aef81b3bc4045e566ddee9808471a17bb55d7
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-09-03 16:03:03 +0900

    feat: 영화목록 API 로직 추가 (중복제거 함께 처리 - lodash 활용) & MovieItem 컴포넌트 로직 추가

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -3,7 +3,8 @@
     <div class="inner">
       <MovieItem
         v-for="movie in movies"
-        :key="movie.imdbID" />
+        :key="movie.imdbID"
+        :movie="movie" />
     </div>
   </div>
 </template>
@@ -14,9 +15,9 @@ export default {
   components: {
     MovieItem
   },
-  data() {
-    return {
-      movies: []
+  computed: {
+    movies() {
+      return this.$store.state.movie.movies
     }
   }
 }
--- src/store/movie.js
+++ src/store/movie.js
@@ -1,23 +1,51 @@
 import axios from 'axios'
+import _uniqBy from 'lodash/uniqBy'
 
 export default {
   namespaced: true,
   state: () => ({
-    movies: []
+    movies: [],
+    message: '',
+    loading: false
   }),
   getters: {},
   mutations: {
+    updateState(state, payload) {
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+      })
+    },
     resetMovies(state) {
       state.movies = []
     }
   },
   actions: {
-    async searchMovies(context, payload) {
+    async searchMovies({ commit, state }, payload) {
       const {title, type, number, year} = payload
       const OMDB_API_KEY = '7035c60c'
 
-      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${number}`)
-      console.log(res)
+      const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
+      const { Search, totalResults } = res.data
+      commit('updateState', {
+        movies: _uniqBy(Search, 'imdbID')
+      })
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10)
+
+      if (pageLength > 1) {
+        for (let page = 2; page <= pageLength; page += 1) {
+          if (page > number / 10) break
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [
+              ...state.movies,
+              ..._uniqBy(Search, 'imdbID')
+            ]
+          })
+        }
+      }
     }
   }
 }
