5a5cd34669f97843c762e2fe91351ef11bf18f51
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-10-05 22:42:55 +0900

    refactor: 영화 검색 API Store 코드 리팩토링

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
     <div class="inner">
+      <div class="message">
+        {{ message }}
+      </div>
       <MovieItem
         v-for="movie in movies"
         :key="movie.imdbID"
@@ -18,6 +21,9 @@ export default {
   computed: {
     movies() {
       return this.$store.state.movie.movies
+    },
+    message() {
+      return this.$store.state.movie.message
     }
   }
 }
