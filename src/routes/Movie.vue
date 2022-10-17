016d7e2d76854850ea8dd14923470b45e116c290
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-10-17 23:36:51 +0900

    feat: 단일 영화 상세 정보 가져오기

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -1,3 +1,13 @@
 <template>
   <h1>Movie!</h1>
 </template>
+<script>
+export default {
+  created() {
+    console.log(this.$route)
+    this.$store.dispatch('movie/searchMovieWithId', {
+      id: this.$route.params.id
+    })
+  }
+}
+</script>
