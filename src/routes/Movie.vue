10fa4f4727cd067ea4d214f99342fa0fa498e032
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-10-19 23:18:34 +0900

    feat: rating 데이터 출력

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -37,6 +37,18 @@
         </div>
         <div class="ratings">
           <h3>Ratings</h3>
+          <div class="rating-wrap">
+            <div
+              v-for="{ Source: name, Value: score } in theMovie.Ratings"
+              :key="name"
+              :title="name"
+              class="rating">
+              <img
+                :src="`https://raw.githubusercontent.com/ParkYoungWoong/vue3-movie-app/master/src/assets/${name}.png`"
+                :alt="name"/>
+              <span>{{ score }}</span>
+            </div>
+          </div>
         </div>
         <div>
           <h3>Actors</h3>
@@ -138,7 +150,7 @@ export default {
     flex-grow: 1;
     .title {
       color: $black;
-      font-family: 'Oswald', sans-serif;
+      font-family: "Oswald", sans-serif;
       font-size: 70px;
       line-height: 1;
       margin-bottom: 30px;
@@ -159,7 +171,19 @@ export default {
       margin-top: 20px;
     }
     .ratings {
-
+      .rating-wrap {
+        display: flex;
+        .rating {
+          display: flex;
+          align-items: center;
+          margin-right: 32px;
+          img {
+            height: 30px;
+            flex-shrink: 0;
+            margin-right: 6px;
+          }
+        }
+      }
     }
     h3 {
       margin: 24px 0 6px;
