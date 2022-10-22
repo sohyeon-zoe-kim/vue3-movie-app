5b625584a8ffa75449d4ea00ff91917319e4dcfc
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-10-22 16:07:13 +0900

    feat: About 페이지 작성 & primary 색상 변경(#ff918d)

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'SOROGI',
+    email: 'hlna030838@gmail.com',
+    blog: 'https://heropy.blog',
+    phone: '+82-10-1234-5678',
+    image: 'https://avatars.githubusercontent.com/u/89514650?v=4'
+  })
+}
