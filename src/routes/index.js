ce567025739a22b37a1e239b141a832174fc510f
sohyeon-zoe-kim <hlna030838@gmail.com>
2022-10-22 16:29:37 +0900

    feat: 404 Page Not Found 추가 & 웹 페이지 타이틀 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './Home'
 import Movie from './Movie'
 import About from './About'
+import NotFound from './NotFound'
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -17,6 +18,10 @@ export default createRouter({
     {
       path: '/about',
       component: About
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
     }
   ]
 })
--- src/store/about.js
+++ src/store/about.js
@@ -3,8 +3,8 @@ export default {
   state: () => ({
     name: 'SOROGI',
     email: 'hlna030838@gmail.com',
-    blog: 'https://heropy.blog',
-    phone: '+82-10-1234-5678',
+    blog: 'https://blog.naver.com/hlna0308',
+    phone: '+82-10-6525-2909',
     image: 'https://avatars.githubusercontent.com/u/89514650?v=4'
   })
 }
